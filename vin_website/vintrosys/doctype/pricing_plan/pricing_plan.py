81d1c4f2fc2df301c9bf067d3313ee770a961eef
kponsakthivel <kponsakthivel@gmail.com>
2026-06-06 15:54:26 +0530

    pricing plan

---
+++ vin_website/vintrosys/doctype/pricing_plan/pricing_plan.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Ponsakthivel and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PricingPlan(Document):
+	pass
